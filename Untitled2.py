81a668f4ae7c627b260c141721c6d8332a9e1340
RonniBert <68221067+RonniBert@users.noreply.github.com>
2020-07-22 10:39:57 +0530

    Add files via upload

---
+++ Untitled2.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello")
+
+
+# In[4]:
+
+
+print("Hello guys")
+
+
+# In[5]:
+
+
+print("Hello\tguys")
+
+
+# In[6]:
+
+
+print("hi 'everyone' hello")
+
+
+# In[7]:
+
+
+name="RAJ"
+age=20
+print("The name is",name,"and age is",age)
+
+
+# In[8]:
+
+
+print("The name is %s and age is %d"%(name,age))
+
+
+# In[9]:
+
+
+x=y=10
+print(x)
+
+
+# In[10]:
+
+
+print(y)
+
+
+# In[11]:
+
+
+print(id(x))
+
+
+# In[12]:
+
+
+a=5**2
+print(a)
+
+
+# In[13]:
+
+
+a=10
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+
+
+# In[14]:
+
+
+print(a!=b)
+
+
+# In[15]:
+
+
+print(a&b)
+print(a|b)
+print(bin(a))
+
+
+# In[16]:
+
+
+print(a>20 or b<5)
+
+
+# In[17]:
+
+
+str="hello"
+print("x" not in str)
+
+
+# In[18]:
+
+
+print(10+10/2*4)
+
+
+# In[19]:
+
+
+print(2**-1)
+
+
+# In[20]:
+
+
+print(10>8>9)
+
+
+# In[ ]:
+
+
+
+
