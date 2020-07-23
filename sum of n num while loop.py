a0833e6be379775d50abad9830292346a2f9143f
RonniBert <68221067+RonniBert@users.noreply.github.com>
2020-07-23 10:51:23 +0530

    Add files via upload

---
+++ sum of n num while loop.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+num = int(input("Enter a number:"))
+
+if num < 0:
+   print("Enter a positive number")
+else:
+   sum = 0
+   while(num > 0):
+       sum += num
+       num -= 1
+   print("The sum is", sum)
+
+
+# In[ ]:
+
+
+
+
