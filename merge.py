d17516152bd63c631f11ae8526d3317375903935
RonniBert <68221067+RonniBert@users.noreply.github.com>
2020-07-27 10:31:48 +0530

    Add files via upload

---
+++ merge.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def Merge(list1, list2): 
+
+    final_list = list1 + list2 
+
+    final_list.sort() 
+
+    return(final_list) 
+list1=[10,20,40,60,70,80]
+list2=[5,15,25,35,45,60]
+print( Merge(list1,list2))
+
+
+# In[ ]:
+
+
+
+
